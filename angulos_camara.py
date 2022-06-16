8706861748b598ebab8fba9bee6dbcde10c88242
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-06-16 23:36:00 +0200

    Update angulos_camara.py
    He acabado el algoritmo para calcular el ángulo de alfa_h.
    He añadido una función para el paso de radianes a grados, y un par que hacen el cambio entre diferentes sistemas de coordenadas.

--- angulos_camara.py
+++ angulos_camara.py
@@ -1,13 +1,34 @@
 # Este breve programa emplea la nomenclatura de la foto "triangulo referencia" para calcular los ángulos de
 # la cámara.
-
+print("\n")
 import math
 
-altura_antena_transmisora = 2.5
-distancia_antena_medida   = 4
+altura_antena_transmisora = 30
+distancia_antena_medida   = 30
 
-angulo_inferior = math.acos(altura_antena_transmisora / distancia_antena_medida)
+def rad2deg(rad):
+    return rad * 180 / math.pi
 
-distancia_pico_transmisor_suelo = altura_antena_transmisora / math.sin(angulo_inferior)
+# Este valor es Alpha_H
+angulo_inferior = math.atan(  altura_antena_transmisora   / distancia_antena_medida)
+print("El ángulo inferior es: ", angulo_inferior, "radianes o ", rad2deg(angulo_inferior), "grados \n")
 
-print("La distancia del pico del transmisor al suelo es:", distancia_pico_transmisor_suelo)
+distancia_transmisor_suelo_receptor = altura_antena_transmisora / math.sin(angulo_inferior)
+print("La distancia del pico del transmisor al suelo es:", distancia_transmisor_suelo_receptor,"\n")
+
+
+# haz una funcion que pase de coordenadas polares a cilindricas
+# make it
+def pol2cart(r, theta):
+    x = r * math.cos(theta)
+    y = r * math.sin(theta)
+    return x, y
+# y otra que pase de coordenadas cilindricas a polares
+# make it
+def cart2pol(x, y):
+    r = math.sqrt(x**2 + y**2)
+    theta = math.atan2(y, x)
+    return r, theta
+
+
+
