c1cbe458e72ef5f47a0b143b53777cc21c6375e8
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-09-13 11:25:43 +0200

    Avance teorico
    He añadido un .txt con dudas.
    
    He añadido un NoteBook de Wolfram para ir escribiendo las ecuaciones.

--- Pruebas/Libreria_Pandas/Interaccion archivos ASCII/manipulacion.py
+++ Pruebas/Libreria_Pandas/Interaccion archivos ASCII/manipulacion.py
@@ -2,12 +2,10 @@
         DEFINICION DEL CODIGO
 """
 from astropy.io import ascii
-from astropy.table import Table
 
 data = ascii.read("archivo.ASC",converters={'': int}, data_start=12)
 
 print(data)
-print(type(data))
 
 
 # https://het.as.utexas.edu/HET/Software/Astropy-1.0/io/ascii/read.html
