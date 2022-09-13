110a8165a09f4d1943c4cd9f40180c6801d388a2
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-09-13 15:46:44 +0200

    Update manipulacion.py
    Extraigo el ASCII en lineas.

--- Pruebas/Libreria_Pandas/Interaccion archivos ASCII/manipulacion.py
+++ Pruebas/Libreria_Pandas/Interaccion archivos ASCII/manipulacion.py
@@ -2,10 +2,12 @@
         DEFINICION DEL CODIGO
 """
 from astropy.io import ascii
+from astropy.table import Table
 
 data = ascii.read("archivo.ASC",converters={'': int}, data_start=12)
 
 print(data)
+print(type(data))
 
 
 # https://het.as.utexas.edu/HET/Software/Astropy-1.0/io/ascii/read.html
@@ -13,3 +15,12 @@ print(data)
 
 # aqui saca las columnas
 #https://www.great-esf.eu/AstroStats13-Python/input_output/asciifiles.html
+
+file = open('archivo.ASC', 'r')
+
+for line in file:
+        line = line.strip()
+        columns = line.split()
+        print(columns)
+
+print(type(columns))
