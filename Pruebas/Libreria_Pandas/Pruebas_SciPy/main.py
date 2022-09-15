810fa4ed75b91f0e3d8e8c64bb600e15c974884a
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-09-15 13:09:44 +0200

    Día de avance teórico
    He avanzado en el entendimiento matemático de lo que tengo que hacer.
    
    Me falta comprender las expresión de una onda esférica.
    
    He añadido 2 papers, uno de Potter que habla sobre las propiedades del uso de ondas esféricas. Otro sobre el paso de NF-FF en esféricas.

--- Pruebas/Libreria_Pandas/Pruebas_SciPy/main.py
+++ Pruebas/Libreria_Pandas/Pruebas_SciPy/main.py
@@ -5,3 +5,10 @@ import numpy as np
 """
 Hankel_orden_2 = scipy.special.hankel2(5,(2+8j), out=None)
 print(Hankel_orden_2)
+
+#https://github.com/scipy/scipy/issues/7722
+def spherical_hn2(n,z,derivative=False):
+    """ Spherical Hankel Function of the Second Kind """
+    return scipy.special.spherical_jn(n,z,derivative=False)-1j*scipy.special.spherical_yn(n,z,derivative=False)
+
+print(spherical_hn2(2,4))
