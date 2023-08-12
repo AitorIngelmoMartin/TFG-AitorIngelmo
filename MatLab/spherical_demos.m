b133c80e45fc28db1e4d716c02bdc6250301d966
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2023-08-12 13:03:41 +0200

    Agregado calculo Bmn con funciones

--- MatLab/spherical_demos.m
+++ MatLab/spherical_demos.m
@@ -18,7 +18,8 @@ delta_phi   = phi(2)-phi(1);
 %Variables campo
 etha = 120/pi;k = 2*pi/lambda;Io=1;l=1;r = 1;z = k*r;
 %Campo
-E_theta = i*etha*((k*Io*l*sin(theta))/(4*pi*r))*(1 +(1/(i*k*r)) - (1/(k*r*k*r)))*exp(-i*k*r);
+E_theta = i*etha*((k*Io*l*sin(theta))/(4*pi*r))*(1 +(1/(i*k*r)) -...
+    (1/(k*r*k*r)))*exp(-i*k*r);
 E_theta = theta;
 E_phi = zeros(1,puntos_del_campo);
 %--------------------------------------------------------------------------
@@ -33,29 +34,27 @@ legendre_deriv = -(1 + n)*cot(theta).*legendre_cos + ...
             (1 + n - abs(m)) * csc(theta) .* legendre_cos_mas;
 
 %--------------------------------------------------------------------------
-%Función esférica de henkel de orden 2
-sphbesselj = ((-1)^(n+1)) * sqrt(pi/(2*z))*besselj(-n-0.5, z);
-sphbessely = ((-1)^(n+1)) * sqrt(pi/(2*z))*bessely(-n-0.5, z);
-h2 =  -sphbessely  - i*sphbesselj;
-
-%--------------------------------------------------------------------------
 %Cálculo de Amn
 
-Amn_parte1 = -i*(2*n+1)/(4*pi*h2)*(factorial(n-m)/factorial(n+m))*(1/(n*(n+1)))*delta_phi*delta_theta;
-
+Amn_parte1 = -i*(2*n+1)/(4*pi*SphericalHankelH2(n,z))*...
+    (factorial(n-m)/factorial(n+m))*(1/(n*(n+1)))*delta_phi*delta_theta;
 Amn_parte2 = sumAcoeff(abs(m), n, theta, phi, E_theta);
-Amn = sum(Amn_parte1*sum(Amn_parte2))
-% -0.00678276 - 0.00147836i
 
+Amn = sum(Amn_parte1*sum(Amn_parte2));
 %--------------------------------------------------------------------------
 %Cálculo de Bmn
 
-Bmn_parte1 = -i*(2*n+1)/(4*pi*h2)*(factorial(n-m)/factorial(n+m))*(1/(n*(n+1)))*delta_phi*delta_theta;
+SphericalHankelH2_derivado = (-SphericalHankelH2(n,z)/(2*z)) + ...
+    1/2*(SphericalHankelH2(-1 + n, z) - SphericalHankelH2(1 + n, z));
 
-Bmn_parte2 = sumAcoeff(abs(m), n, theta, phi, E_theta);
-Bmn = sum(Bmn_parte1*sum(Bmn_parte2))
-% -0.00678276 - 0.00147836i
+Bmn_parte1 = (2*n+1)/(4*pi*SphericalHankelH2_derivado+(1/(z))*...
+    SphericalHankelH2(n,z))*(factorial(n-m)/factorial(n+m))*...
+    (1/(n*(n+1)))*delta_phi*delta_theta;
+Bmn_parte2 = sumBcoeff(abs(m), n, theta, phi, E_theta);
 
+Bmn = sum(Bmn_parte1*sum(Bmn_parte2));
+
+% -0.486479 - 0.343352i
 
 
 
