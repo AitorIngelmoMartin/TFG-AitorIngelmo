4396c7dc72c316c45cf07b449dbf28d9404f3987
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2023-08-12 13:03:19 +0200

    Identación de la función sumAcoeff

--- MatLab/sumAcoeff.m
+++ MatLab/sumAcoeff.m
@@ -5,7 +5,8 @@ function result = sumAcoeff(m, n, theta, phi, Etheta)
     for mtheta = 1:Mtheta
         legendre_cos = legendre(n,cos(theta(mtheta)));
         for nphi = 1:Nphi
-            result(mtheta,nphi) = (Etheta(mtheta) * m * legendre_cos(m+1)) * exp(-1i * m * phi(nphi));
+            result(mtheta,nphi) = (Etheta(mtheta) * m * legendre_cos(m+1))...
+                * exp(-1i * m * phi(nphi));
         end
     end
 end
