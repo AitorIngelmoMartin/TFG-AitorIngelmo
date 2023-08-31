0fab23a33160f5c8810de9265fbc3264d10d5aa8
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2023-08-31 17:34:14 +0200

    Programado en python funciones para csc, cot y LegendrePDerivadaCos

--- Codigos/Hansen/pruebas_recursos_hansen.py
+++ Codigos/Hansen/pruebas_recursos_hansen.py
@@ -1,6 +1,7 @@
 import scipy
+import numpy as np
 from scipy import special
-from numpy import sin, cos
+from numpy import sin, cos, sign
 
 def legendre_polinom(n,m,z):
     """Function that calculate the legendre polinom"""
@@ -17,6 +18,28 @@ def legendre_division(n, m, theta):
     """Function that calculate a fraction that can be NaN"""
     return -0.5*(legendre_polinom(n + 1, m + 1,cos(theta)) + (n - m + 1)*(-n + m + 2)*legendre_polinom(n + 1, m - 1, cos(theta)))
 
+def cot(rad):
+    """Function that make the cotang of an angle given in radians"""
+    return -np.tan(rad + np.pi/2)
 
-print(legendre_division(2,1,3.151617))
+def cosec(rad):
+    """Dunction that calculate the cosecant of an angle given in radians"""
+    return 1/sin(rad)
 
+def legendre_cos_derivate(n,m,theta):
+    """Function that calculate the cos(x) partial derivate of legendre polinom"""
+    if not theta == 0 and not theta == np.pi:
+        return -(1 + n)*cot(theta)*legendre_polinom(n, np.abs(m), cos(theta)) + \
+            (1 + n - np.abs(m))*cosec(theta)*legendre_polinom(n+1, np.abs(m), cos(theta))
+
+def expresion_sign(m):
+    """Function that save the a NaN related with 0^0"""
+    if m == 0:
+        return -1
+    return -sign(m)**np.abs(m)
+
+def Ksmn(s, n, m, theta, phi):
+    """Function that calculate the Ksmn expresion from hankel book"""
+    if s == 1:
+        np.sqrt(2/(2*(n+1)))*expresion_sign(m)* np.exp(1j * m * phi) * (-1j)**(n+1) *np.array[legendre_division(n, m, theta),-legendre_cos_derivate(n, m, theta)]
+print(legendre_cos_derivate(2,2,5))
