43a63e8dd026d6be11cd05ba7f6564a2991a04e9
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2024-05-29 20:30:33 +0200

    Cambio del enmascarado para usar funciones numoy + inicio de la creacion de interpoladores

--- NFtoFF-Trasnformacion JL/Codigos/transformation/transformation.py
+++ NFtoFF-Trasnformacion JL/Codigos/transformation/transformation.py
@@ -2,8 +2,10 @@
 import os, sys
 import numpy as np
 import numpy.ma as ma
-import matplotlib.pyplot as plt
 from math import cos, sin
+import matplotlib.pyplot as plt
+from scipy.interpolate import RegularGridInterpolator
+
 
 # Import 'funciones' module
 current = os.path.dirname(os.path.realpath(__file__))
@@ -17,7 +19,7 @@ c0            = 299792458
 raw_data      = 9
 pauseinterval = 0.01
 threshold     = 1e-10
-delta_x, delta_y, coordz = 0 , 0, 0
+delta_x, delta_y = 0 , 0
 file_type = ['Ex','Ey','Ez','normE']
 
 flow_config = { 
@@ -40,7 +42,7 @@ def input_data_process(flow_config: dict):
     """Function used to save inputs from our flow_config dictionary"""
 
     try:
-        fields,files,datavalues,lines,zValueplane,zValueMaskedplane,zValueZeroedplane = {}, {}, {}, {}, {} , {}, {}     
+        fields,files = {}, {}
         if len(flow_config['file_type']) == len(flow_config['files_in_directory']):
             for file_type in range(len(flow_config['file_type'])):
                 files.update({flow_config['file_type'][file_type]:f"{flow_config['directory']}//{flow_config['files_in_directory'][file_type]}"})
@@ -48,14 +50,11 @@ def input_data_process(flow_config: dict):
             raise Exception("El número de ficheros en el directorio es distinto a la cantidad de ficheros introducido")
         fields.update({'files':files})
         fields.update({'file_type':flow_config['file_type']})
-        fields.update({"datavalues":datavalues})
-        fields.update({"lines":lines})
-        fields.update({"zValueplane":zValueplane}) 
-        fields.update({"zValueMaskedplane":zValueMaskedplane}) 
-        fields.update({"zValueZeroedplane":zValueZeroedplane})
+        fields.update({"field_readed":{}})
+        fields.update({"lines":{}})
+        fields.update({"field_readed_masked":{}})
         fields.update({'fields_transformed':{}})
-        fields.update({'quantitative_comparison':{}})          
-        fields.update({'shape':flow_config['shape']})
+        fields.update({'quantitative_comparison':{}})
         fields.update({'freq':flow_config['freq']})    
         fields.update({'length_unit':flow_config['length_unit']})
         fields.update({'res':flow_config['res']})
@@ -84,7 +83,7 @@ def extract_matrix_data(fields):
     """
     Este método almacena en arrays los datos de los ficheros de Comsol leídos previamente con readData             
     """
-    global delta_x, delta_y, coordz
+    global delta_x, delta_y
     #filetypes = lines.keys()
     coord_flag = None
     for file_type in fields['lines'].keys(): #file_type
@@ -100,43 +99,64 @@ def extract_matrix_data(fields):
 
             coordy  = np.unique(coord[:,1])*fields['length_unit']
             delta_y = coordy[1]-coordy[0]
-            coordz  = np.unique(coord[:,2])*fields['length_unit']
-        fields['datavalues'][datatype] = np.array([complex(s.replace('i', 'j')) for i in range(len(rawdatalines)) \
+
+        fields['field_readed'][datatype] = np.array([complex(s.replace('i', 'j')) for i in range(len(rawdatalines)) \
             for k,s in zip(range(4),rawdatalines[i].split()) if k == 3])
 
-def extract_phi_value_cut(fields,field_components,cuts_to_extract):
-    """
-    Este método nos permite extraer los valores del campo en un cierto número de cortes o valores de z 
-        "fields": Es el diccionario que contiene los datos que estamos tratando en el programa.
-        "field_components":Es el array que contiene todas las componentes que sobre las cuales 
-                           queremos extraer los cortes.
-        "cuts_to_extract" : Es el array que contiene los cortes (medidos en metros) que deseamos extraer.       
-    """    
-    numberOfCuts = len(cuts_to_extract)
-    shape_0      = fields['shape'][0]
-    shape_1      = fields['shape'][1]
-
-    if numberOfCuts == 1:
-        indexarray = list(np.where(np.abs(coordz-cuts_to_extract)<fields['res'])[0])
-    else:
-        indexarray = [np.where(np.abs(coordz-cuts_to_extract[i])<fields['res'])[0][0] for i in range(numberOfCuts)]
-    position0 = np.array([shape_0*shape_1*indexarray[i] for i in range(numberOfCuts)])
-    indices   = [range(position0[i],position0[i]+shape_0*shape_1) for i in range(numberOfCuts)]
-    
-    for field_component in field_components:
-        field_component_value = fields['datavalues'][field_component]
-        fields['zValueplane'][field_component] = np.array([field_component_value[indices[i]] for i in range(numberOfCuts)]).reshape(numberOfCuts,shape_0,shape_1)
-
-def mask_cut(fields,datatypes):
+def mask_values(fields,datatypes):
     for datatype in datatypes:
-        fields['zValueZeroedplane'][datatype] = np.nan_to_num(fields['zValueplane'][datatype], nan=0)
+        field_component_with_no_nan = np.nan_to_num(fields['field_readed'][datatype], nan=0)
+        fields['field_readed_masked'][datatype] = field_component_with_no_nan[np.nonzero(field_component_with_no_nan)]
+
+def change_coordinate_system_to_spherical(fields: dict):
+    """Function used to change the coordinate system from cartesians to sphericals"""
+
+    make_interpolator(fields['field_readed_masked']['Ex'],1,5,0,np.pi,0,2*np.pi,100)
+
+def make_interpolator(field_component, r_init: int, r_end: int, theta_init: int, theta_end: int, phi_init: int, phi_end: int, number_of_values: int):
+    """Function used to make an interpolator to obtain spherical values"""
+
+    # Creation of base coordinates
+    # r = np.arange(r_init, r_end)
+    r = 1
+    theta = np.linspace(theta_init, theta_end, number_of_values)
+    phi = np.linspace(phi_init, phi_end, number_of_values)
+
+    spherical_field_component = []
+    for field_componenet_value in field_component:
+        spherical_field_component.append(field_componenet_value*(np.sin(theta)*np.cos(phi) + np.sin(theta)*np.sin(phi) + np.cos(theta)))
+    print(spherical_field_component)
+    # Creation of the interpolator
+    # interpolador = RegularGridInterpolator((r, theta, phi), valores, bounds_error=False, fill_value=None)
+
+if __name__ == '__main__':
+    plt.close('all')
+    #try:
+
+    fields = input_data_process(flow_config)
+    
+    read_data(fields)    
+    
+    extract_matrix_data(fields)
+    
+
+    # Este método quita de en medio los NaN.
+    mask_values(fields,fields['file_type'])
+
+    change_coordinate_system_to_spherical(fields)
+    # near_field_to_far_field_transformation(fields)
+    
+    print("FIN PROGRAMA")
+ 
+    #except Exception as exc:
+        #print(exc)
 
 
 def near_field_to_far_field_transformation(fields: dict, cut: int):
-    Nx = fields['zValueZeroedplane']['Ex'][cut].shape[0]
-    Ny = fields['zValueZeroedplane']['Ex'][cut].shape[1]
+    Nx = fields['field_readed_masked']['Ex'][cut].shape[0]
+    Ny = fields['field_readed_masked']['Ex'][cut].shape[1]
     num = 0
-    for value in fields['zValueZeroedplane']['Ex'][cut]:
+    for value in fields['field_readed_masked']['Ex'][cut]:
 
         emn_calculated = calculate_emn_from_dipole_field()
 
@@ -153,27 +173,3 @@ def calculate_emn_from_dipole_field(number_of_points: int, m: int, n: int, r: in
             # total_result += [e_dipole_r_field(r, theta, k, eta, l, Io),e_dipole_theta_field(r,theta,k,eta,l,Io),0]*funciones.b_sin_function(-m,n,theta,phi)
             total_result += np.array([0,e_dipole_theta_field(r,theta,k,eta,l,Io),0])*funciones.b_sin_function(-m,n,theta,phi)
     return total_result*delta_theta*delta_phi
-
-
-if __name__ == '__main__':
-    plt.close('all')
-    #try:
-
-    fields = input_data_process(flow_config)
-    
-    read_data(fields)    
-    
-    extract_matrix_data(fields)
-
-    extract_phi_value_cut(fields,fields['file_type'],cuts_to_extract = [15.e-3,30.e-3])
-    
-    # #Este método quita de en medio los NaN.
-    mask_cut(fields,fields['file_type'])
-
-    near_field_to_far_field_transformation(fields, cut=0)
-
-    print("FIN PROGRAMA")
- 
-    #except Exception as exc:
-        #print(exc)
-
